642da8643d07a18d89f1f717b6dda73039c1f019
Guanjie <gjsun@666.com>
2024-07-29 10:49:33 +0800

    complte the extraction from pdf

--- extract_pdf.py
+++ extract_pdf.py
@@ -1,9 +1,9 @@
-# 提取pdf文件的一个切片
-
+import os
 from PyPDF2 import PdfReader, PdfWriter
 
 
 def extract_pages(input_path, output_path, start_page, end_page):
+    """提取pdf文件的一个切片"""
     reader = PdfReader(input_path)
     writer = PdfWriter()
 
@@ -18,14 +18,17 @@ def extract_pages(input_path, output_path, start_page, end_page):
 
 
 def main():
-    # 使用示例
-    input_pdf = "input.pdf"  # 输入PDF文件路径
-    output_pdf = "output.pdf"  # 输出PDF文件路径
-    start_page = 33  # 开始页码
-    end_page = 65  # 结束页码
-
-    extract_pages(input_pdf, output_pdf, start_page, end_page)
-    print(f"已从第 {start_page} 页到第 {end_page} 页提取并保存到 {output_pdf}")
+    # 获取当前脚本所在目录
+    current_dir = os.path.dirname(os.path.abspath(__file__))
+    # 设置输入和输出文件路径
+    pdf_folder = os.path.join(current_dir, "PDF Files")
+    input_pdf_path = os.path.join(pdf_folder, "Rough Sets.pdf")
+    output_pdf_path = os.path.join(pdf_folder, "A Framework of Three-Way Cluster Analysis.pdf")
+    start_page = 324  # 开始页码
+    end_page = 336  # 结束页码
+    print(input_pdf_path)
+    print(output_pdf_path)
+    extract_pages(input_pdf_path, output_pdf_path, start_page, end_page)
 
 
 if __name__ == '__main__':
