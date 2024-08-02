cf76d7c01d144df287aa4e6149ec1702ab42f9ef
Guanjie <gjsun@666.com>
2024-08-02 16:28:44 +0800

    test a 3WK-Means algorithm

--- kmeans.py
+++ kmeans.py
@@ -1,22 +1,3 @@
-"""
-熟悉numpy中常用函数和numpy的广播机制
-
-1. np.random.choice, np.argmin, np.all, np.any, np.full
-2. 关于arr的shape=(2, 3, 4)
-    理解为：共两页，每页三行，每行四列
-    也即：共2个二维数组，每个二维数组有3个一维数组，每个一维数组有4个元素
-3. np的广播机制的基本原则：首先看维度数是否相同！！！
-    - 如果两个数组的维度数不同，形状较小的数组会在前面补充维度为1
-    - 若两个数组的维度数相同，且两个数组的形状在某个维度上不匹配，但其中一个的长度为1，则该维度会被扩展以匹配另一个数组
-    - 若两个数组的维度数相同，且两个数组的形状在某个维度上不匹配，但两者都不为1，则会报错
-    如
-4. np的增维原则
-    假设a = [[4, 9, 9, 4], [5, 6, 6, 7]]), a的形状是(2, 4)
-    则b = a[:, np.newaxis, :]的形状就是(2, 1, 4)
-    则b =[[[4, 9, 9, 4]], [[5, 6, 6, 7]]]
-"""
-
-
 import numpy as np
 import matplotlib.pyplot as plt
 
@@ -34,17 +15,17 @@ def kmeans(data: np.ndarray, k: int = 3, max_iterations: int = 100, tolerance: f
         - labels: np.ndarray, shape=(n_samples,), cluster labels for each sample
     """
     n_samples = data.shape[0]
-    np.random.seed(n_samples)
-    if not isinstance(data, np.ndarray) or data.ndim != 2:
-        # 判断data是否有效
+    np.random.seed(100)
+
+    if not isinstance(data, np.ndarray) or data.ndim != 2:  # 判断data是否有效
         raise ValueError("Data must be a 2D numpy array")
     if k <= 0 or k > n_samples:
-        # 判断k是否有效
-        raise ValueError("Invalid number of clusters")
-    centroids = data[np.random.choice(n_samples, k, replace=False)]  # 初始化聚类中心
-    labels = np.full(n_samples, -1)  # 样本的簇标签初始化为-1
+        raise ValueError("Invalid number of clusters")  # 判断k是否有效
+
+    centroids = data[np.random.choice(n_samples, k, replace=False)]  # 初始化k个聚类中心
+    labels = np.full(n_samples, -1)  # 数据点的簇标签初始化为-1
     for _ in range(max_iterations):
-        # distances是一个形状为(k, n_samples)的数组，表示每一个聚类中心和其他所有点的距离，理解numpy广播机制
+        # distances是一个形状为(k, n_samples)的np.ndarray实例，表示每一个聚类中心和其他所有点的距离
         distances = np.sqrt(np.sum(np.square(data-centroids[:, np.newaxis, :]), axis=2))
         labels = np.argmin(distances, axis=0)
         # 更新聚类中心
@@ -57,19 +38,77 @@ def kmeans(data: np.ndarray, k: int = 3, max_iterations: int = 100, tolerance: f
     return centroids, labels
 
 
+def three_way_kmeans(data: np.ndarray, k: int = 3, max_iterations: int = 100, tolerance=1e-4, epsilon=2.0):
+    n_samples = len(data)
+    np.random.seed(100)
+    centroids = data[np.random.choice(n_samples, k, replace=False)]  # 初始化k个聚类中心
+    for _ in range(max_iterations):
+        # distances是一个形状为(k, n_samples)的np.ndarray实例，表示每一个聚类中心和其他所有点的距离
+        distances = np.sqrt(np.sum(np.square(data-centroids[:, np.newaxis, :]), axis=2))
+        judge = distances - np.min(distances, axis=0)
+        bool_judge = judge < epsilon
+        # 计算每一个簇包含的数据点
+        clusters = list()
+        for i in range(k):
+            cluster = np.where(bool_judge[i, :] == 1)[0]
+            clusters.append(cluster)
+        return centroids, clusters
+
+
 def visualization_before_clustering(data, ax):
     # 在聚类之前可视化数据分布
-    ax.scatter(data[:, 0], data[:, 1])
+    ax.scatter(data[:, 0], data[:, 1], s=5, marker='.', color='black')
     ax.set_title('Visualization Before Clustering')
     ax.set_xlabel('Feature 1')
     ax.set_ylabel('Feature 2')
 
 
-def visualization_after_clustering(data, centroids, labels, ax):
+def visualization_after_kmeans_clustering(data, centroids, labels, ax):
     # 聚类结果可视化
-    ax.scatter(data[:, 0], data[:, 1], c=labels, cmap='viridis')
-    ax.scatter(centroids[:, 0], centroids[:, 1], c='red', s=200, marker='*')
-    ax.set_title('Visualization After Clustering')
+    ax.scatter(data[:, 0], data[:, 1], c=labels, cmap='viridis', s=5, marker='.')
+    ax.scatter(centroids[:, 0], centroids[:, 1], color='red', s=20, marker='.')
+    ax.set_title('Visualization After K-Means Clustering')
+    ax.set_xlabel('Feature 1')
+    ax.set_ylabel('Feature 2')
+
+
+def visualization_after_three_way_kmeans_clustering(data, centroids, clusters, ax):
+    # 三支聚类结果可视化
+    k = len(clusters)  # k保存簇的个数
+    clusters = [set(clusters[i]) for i in range(k)]  # clusters[np.ndarray] -> clusters[set]
+    cores = [set() for _ in range(k)]
+    fringes = [set() for _ in range(k)]
+    for i in range(k):
+        m, n = (i+1) % k, (i+2) % k
+        # 簇i核心域的样本点只能在i中，不能在簇(i+1)%k中，也不能在簇(i+2)%k中
+        cores[i] = (clusters[i].difference(clusters[m])).intersection(clusters[i].difference(clusters[n]))
+        # cores[i]和fringes[i]的并集就是cores[i]，也就是簇i的支集
+        fringes[i] = clusters[i].difference(cores[i])
+
+    # 给核心域的数据点打上簇标签
+    labels = list()
+    for i in range(k):
+        label = [i for _ in cores[i]]
+        labels.append(label)
+
+    # 绘制核心域的点
+    for i in range(k):
+        colors = ['yellow', 'green', 'blue']
+        core_i_data = data[list(cores[i])]  # 集合不可以做np.ndarray的索引，列表可以
+        ax.scatter(core_i_data[:, 0], core_i_data[:, 1], c=colors[i], marker='.', s=5)
+
+    # 绘制边缘域的点
+    a = (fringes[0]).union(fringes[1])
+    a = a.union(fringes[1])
+    a = a.union(fringes[2])
+    fringe_data = data[list(a)]
+    ax.scatter(fringe_data[:, 0], fringe_data[:, 1], c='black', s=20, marker='.')
+
+    # 绘制聚类中心
+    ax.scatter(centroids[:, 0], centroids[:, 1], c='red', s=20, marker='.')
+
+    # 设置图表标题，x轴标题，y轴标题
+    ax.set_title('Visualization After 3WK-Means Clustering')
     ax.set_xlabel('Feature 1')
     ax.set_ylabel('Feature 2')
 
@@ -77,12 +116,19 @@ def visualization_after_clustering(data, centroids, labels, ax):
 def main():
     # 生成数据
     data = np.loadtxt('sample.txt')
-    # 获取聚类结果：每一个簇的中心；每一个样本的簇标签
+
+    # 获取K-Means聚类结果
     centroids, labels = kmeans(data, k=3, max_iterations=1000, tolerance=1e-4)
+    # 获取3WK-Means聚类结果
+    centers, clusters = three_way_kmeans(data, k=3, max_iterations=1000, tolerance=1e-4)
+
     # 数据可视化
-    fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(12, 6))
+    fig, (ax1, ax2, ax3) = plt.subplots(1, 3, figsize=(12, 4))
     visualization_before_clustering(data, ax1)
-    visualization_after_clustering(data, centroids, labels, ax2)
+    visualization_after_kmeans_clustering(data, centroids, labels, ax2)
+    visualization_after_three_way_kmeans_clustering(data, centers, clusters, ax3)
+
+    # 图片布局设置
     fig.tight_layout()
     plt.show()
 
