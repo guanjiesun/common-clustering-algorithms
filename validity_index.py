58f629e77eb1476f476f239e6268b71c3923daaf
Guanjie <gjsun@666.com>
2024-08-06 20:07:22 +0800

    design a function named calculate_dunn

--- validity_index.py
+++ validity_index.py
@@ -1,6 +1,6 @@
 import numpy as np
 import pandas as pd
-from scipy.spatial.distance import cdist
+from validclust import dunn
 from sklearn.metrics import pairwise_distances
 from sklearn.metrics import silhouette_score as asi
 from sklearn.metrics import davies_bouldin_score as dbi
@@ -86,7 +86,7 @@ def calculate_silhouette_score(data: np.ndarray, clusters: list[np.ndarray]) -> 
         for j in range(k):
             if j != i:
                 # distance存储簇i的所有点到簇j的所有点的距离
-                distance = cdist(data[cores[i]], data[cores[j]])
+                distance = pairwise_distances(data[cores[i]], data[cores[j]])
                 distances.append(np.sum(distance, axis=1) / len(cores[j]))
 
         distances = np.array(distances)
@@ -106,9 +106,12 @@ def calculate_silhouette_score(data: np.ndarray, clusters: list[np.ndarray]) -> 
 
 
 def calculate_chi(data: np.ndarray, clusters: list[np.ndarray]) -> float:
-    # TODO 此函数的计算结果和sklearn.metrics.calinski_harabasz_score函数的结果一致！！！
-    # 计算calinski harabasz score(方差比准则, 簇间紧密度与簇内紧密度的比值)
+    """
+    TODO 此函数的计算结果和sklearn.metrics.calinski_harabasz_score函数的结果一致！！！
+    计算calinski harabasz score(方差比准则, 簇间紧密度与簇内紧密度的比值)
+    """
     k = len(clusters)
+
     # TODO 让每一个簇的核心域参与计算，不要包括边缘域
     # 如果clusters是K-Means产生的，那么cores和clusters相同(因为边界域是空集)
     _, cores, _ = get_cores_fringes(clusters)
@@ -149,12 +152,39 @@ def calculate_chi(data: np.ndarray, clusters: list[np.ndarray]) -> float:
     return (np.trace(b_matrices) / (k-1)) / (np.trace(w_matrices) / (n-k))
 
 
+def calculate_dunn(data: np.ndarray, clusters: list[np.ndarray]) -> float:
+    """
+    TODO 此函数的计算结果和validclust.dunn函数的结果一致！！！
+    dunn指数的值越大表示聚类效果越好; dunn越大, 簇间距离大 (簇分离得好) 且簇内距离小 (簇紧密)
+    """
+    k = len(clusters)
+
+    # TODO 让每一个簇的核心域参与计算，不要包括边缘域
+    # 如果clusters是K-Means产生的，那么cores和clusters相同(因为边界域是空集)
+    _, cores, _ = get_cores_fringes(clusters)
+
+    # 计算每一对簇之间的簇间距离(用簇i和簇j最近样本间的距离表示)
+    intra_dists = list()
+    for i in range(k):
+        for j in range(i, k):
+            if j != i:
+                intra_dists.append(np.min(pairwise_distances(data[cores[i]], data[cores[j]])))
+
+    # 计算每一个簇的簇内距离
+    inter_dists = list()
+    for i in range(k):
+        inter_dists.append(np.max(pairwise_distances(data[cores[i]])))
+
+    # 计算dunn值
+    return np.min(intra_dists) / np.max(inter_dists)
+
+
 def main() -> None:
     """计算K-Means和3WK-Means算法的DBI"""
     # 导入数据
     data = np.loadtxt('sample.txt')
 
-    # 对于K-Means, get_labels_data返回的数据集就是data的复制品
+    # 对于K-Means, get_labels_data返回的数据集就是data的副本
     _, clusters = kms(data, k=3)
     _, labels = get_data_labels(data, clusters)
 
@@ -162,31 +192,37 @@ def main() -> None:
     _, clusters_3w = twkms(data, k=3, epsilon=2.64)
     cores_data, cores_labels = get_data_labels(data, clusters_3w)
 
-    # 计算DBI
+    # 使用自己写的函数计算K-Means的validity indices
     dbi_kms = calculate_dbi(data, clusters)
-    dbi_kms_std = dbi(data, labels)
-    dbi_3w = calculate_dbi(data, clusters_3w)
-    dbi_3w_std = dbi(cores_data, cores_labels)
-
-    # 计算ASI
     asi_kms = calculate_silhouette_score(data, clusters)
-    asi_kms_std = asi(data, labels)
-    asi_3w = calculate_silhouette_score(data, clusters_3w)
-    asi_3w_std = asi(cores_data, cores_labels)
-
-    # 计算CHI
     chi_kms = calculate_chi(data, clusters)
+    dunn_kms = calculate_dunn(data, clusters)
+
+    # 使用第三方库函数计算K-Means的validity indices
+    dbi_kms_std = dbi(data, labels)
+    asi_kms_std = asi(data, labels)
     chi_kms_std = chi(data, labels)
+    dunn_kms_std = dunn(pairwise_distances(data), labels)
+
+    # 使用自己写的函数计算3WK-Means的validity indices
+    dbi_3w = calculate_dbi(data, clusters_3w)
+    asi_3w = calculate_silhouette_score(data, clusters_3w)
     chi_3w = calculate_chi(data, clusters_3w)
+    dunn_3w = calculate_dunn(data, clusters_3w)
+
+    # 使用第三方库函数计算3WK-Means的validity indices
+    dbi_3w_std = dbi(cores_data, cores_labels)
+    asi_3w_std = asi(cores_data, cores_labels)
     chi_3w_std = chi(cores_data, cores_labels)
+    dunn_3w_std = dunn(pairwise_distances(cores_data), cores_labels)
 
     # 设置字典和DataFrame
-    index = ['DBI', 'ASI', 'CHI']
+    index = ['DBI', 'ASI', 'CHI', 'DUNN']
     dictionary = {
-        "K-Means(std)": np.round([dbi_kms_std, asi_kms_std, chi_kms_std], 4),
-        "K-Means": np.round([dbi_kms, asi_kms, chi_kms], 4),
-        "3WK-Means": np.round([dbi_3w, asi_3w, chi_3w], 4),
-        "3WK-Means(std)": np.round([dbi_3w_std, asi_3w_std, chi_3w_std], 4),
+        "K-Means(std)": np.round([dbi_kms_std, asi_kms_std, chi_kms_std, dunn_kms_std], 4),
+        "K-Means": np.round([dbi_kms, asi_kms, chi_kms, dunn_kms], 4),
+        "3WK-Means": np.round([dbi_3w, asi_3w, chi_3w, dunn_3w], 4),
+        "3WK-Means(std)": np.round([dbi_3w_std, asi_3w_std, chi_3w_std, dunn_3w_std], 4),
     }
     df = pd.DataFrame(dictionary, index=index)
     print(df)
