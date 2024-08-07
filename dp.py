5d224b0a3104d8fdde863c2d37f1a62dfaf9d335
Guanjie <gjsun@666.com>
2024-08-07 22:51:34 +0800

    nothing special

--- dp.py
+++ dp.py
@@ -160,6 +160,10 @@ def draw_cluster(data, labels, centroids, dic_colors):
         plt.scatter(sub_delta[:, 0], sub_delta[:, 1], s=15, color=dic_colors[i])
         # 画聚类中心
         plt.scatter(data[centroids[i], 0], data[centroids[i], 1], color='black', marker="x", s=100)
+
+    foo = np.where(labels == -1)[0]
+    other_data = data[foo]
+    plt.scatter(other_data[:, 0], other_data[:, 1], s=15, color='black')
     plt.show()
 
 
