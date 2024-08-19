0eeb3831fb1705d016b16909573112866269dade
Guanjie <gjsun@666.com>
2024-08-19 21:55:54 +0800

    optimize code

--- dbscan.py
+++ dbscan.py
@@ -29,7 +29,7 @@ def dbscan(dataset: np.ndarray, eps: float, min_samples: int) -> np.ndarray:
             labels[i] = -1  # 噪声点的标签为-1
             continue
 
-        # 如果样本点i既没有被处理过，也不是噪声点
+        # 如果样本点i既没有被处理过，也不是噪声点，那么i就是核心点，给i分配一个簇标签
         labels[i] = c
 
         # 获取样本点i的种子集合(不包含i)
@@ -40,9 +40,9 @@ def dbscan(dataset: np.ndarray, eps: float, min_samples: int) -> np.ndarray:
         while seed_set:
             j = seed_set.pop()
             if labels[j] == -1:
-                labels[j] = c
+                labels[j] = c  # 如果i的邻域j之前的标签是-1，那么让j的标签和i保持一致，即让j变成边界点
             if labels[j] != -2:
-                continue
+                continue  # 如果j已经被处理过，则跳过循环，继续处理样本点i的其他邻域点
 
             labels[j] = c
             j_neighbors = range_query(dataset, j, eps)
@@ -56,20 +56,32 @@ def dbscan(dataset: np.ndarray, eps: float, min_samples: int) -> np.ndarray:
 
 def visualize_dbscan_result(dataset: np.ndarray, labels: np.ndarray) -> None:
     """可视化sklean中的DBSCAN算法"""
-    print(np.unique(labels))
-    plt.scatter(dataset[:, 0], dataset[:, 1], c=labels + 1, cmap='viridis', s=15, marker='o')
+    plt.scatter(dataset[:, 0], dataset[:, 1], c=labels, cmap='plasma', s=15, marker='o')
     plt.show()
 
 
 def main():
-    dataset = np.loadtxt('sample.txt')
+    # dataset = np.loadtxt('sample.txt')  # 0.5, 5
+    import pandas as pd
+    # dataset = pd.read_csv('./datasets/D5.csv').to_numpy()  # 0.5, 5
+    # dataset = pd.read_csv('./datasets/D6.csv').to_numpy()  # 1, 5
+    # dataset = pd.read_csv('./datasets/D7.csv').to_numpy()  # 6, 5
+    dataset = pd.read_csv('./datasets/D8.csv').to_numpy()  # 9, 5
+    # dataset = pd.read_csv('./datasets/D9.csv').to_numpy()  # 0.90, 5
+    # dataset = pd.read_csv('./datasets/D10.csv').to_numpy()  # 0.15, 5
+    # dataset = pd.read_csv('./datasets/D11.csv').to_numpy()  # 0.15, 5
+    # dataset = pd.read_csv('./datasets/D12.csv').to_numpy()  # 0.5, 5
+
+    eps, min_samples = 9, 5
 
     # 使用sklearn中的DBSCAN算法
     # noinspection PyUnresolvedReferences
-    labels_std = DBSCAN(eps=0.5, min_samples=5).fit(dataset).labels_
+    labels_std = DBSCAN(eps=eps, min_samples=min_samples).fit(dataset).labels_
+    print(np.unique(labels_std))
 
     # 使用自己实现的DBSCAN算法
-    labels = dbscan(dataset, eps=0.5, min_samples=5)
+    labels = dbscan(dataset, eps=eps, min_samples=min_samples)
+    print(np.unique(labels))
 
     # 可视化聚类结果
     visualize_dbscan_result(dataset, labels_std)
