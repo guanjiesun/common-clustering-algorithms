e24309833d45a6162985b5d8d35a40cb17d7cd9b
Guanjie <gjsun@666.com>
2024-08-27 17:04:44 +0800

    optimize code

--- gbs.py
+++ gbs.py
@@ -186,10 +186,10 @@ def visualize_gbs(gbs: list[GranularBall]) -> None:
     plt.show()
 
 
-def visualize_original_data(data: np.ndarray) -> None:
+def visualize_original_data(dataset: np.ndarray) -> None:
     """可视化原始数据"""
     fig, ax = plt.subplots()
-    ax.scatter(data[:, 0], data[:, 1], s=5, marker='.', color='black')
+    ax.scatter(dataset[:, 0], dataset[:, 1], s=5, marker='.', color='black')
     ax.set_title('Original Data')
     ax.set_aspect('equal', adjustable='box')
     plt.show()
