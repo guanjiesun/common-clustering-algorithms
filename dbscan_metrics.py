156a81eb542f44757aa81770e9c5d54a23f6220b
Guanjie <gjsun@666.com>
2024-08-28 19:14:54 +0800

    optimize code

--- dbscan_metrics.py
+++ dbscan_metrics.py
@@ -1,8 +1,10 @@
 import numpy as np
-from sklearn.datasets import make_blobs
-from sklearn.cluster import DBSCAN
-from sklearn.metrics import silhouette_score, calinski_harabasz_score, davies_bouldin_score
 import matplotlib.pyplot as plt
+from sklearn.cluster import DBSCAN
+from sklearn.datasets import make_blobs
+from sklearn.metrics import silhouette_score, calinski_harabasz_score, davies_bouldin_score
+from sklearn.metrics import pairwise_distances
+from validclust import dunn
 
 
 def calculate_clustering_metrics(data: np.ndarray, labels: np.ndarray) -> None:
@@ -11,9 +13,11 @@ def calculate_clustering_metrics(data: np.ndarray, labels: np.ndarray) -> None:
         silhouette = silhouette_score(data, labels)
         calinski_harabasz = calinski_harabasz_score(data, labels)
         davies_bouldin = davies_bouldin_score(data, labels)
+        dunn_score = dunn(pairwise_distances(data), labels)
         print(f"Silhouette Score: {silhouette:.4f}")
         print(f"Calinski-Harabasz Index: {calinski_harabasz:.4f}")
         print(f"Davies-Bouldin Index: {davies_bouldin:.4f}")
+        print(f"Dunn Index: {dunn_score:.4f}")
     else:
         print("Not enough clusters to calculate metrics.")
 
@@ -49,19 +53,19 @@ def main():
     dbscan = DBSCAN(eps=0.5, min_samples=5)
     labels = dbscan.fit_predict(data)
 
-    # # 计算内部聚类指标(排除噪声点)
-    # valid_points = labels != -1
-    # valid_data = data[valid_points]
-    # valid_labels = labels[valid_points]
+    # 计算内部聚类指标(排除噪声点)
+    valid_points = labels != -1
+    valid_data = data[valid_points]
+    valid_labels = labels[valid_points]
+
+    # 计算DBSCAN聚类指标(不包含噪声点)
+    calculate_clustering_metrics(valid_data, valid_labels)
+
+    # 计算DBSCAN聚类指标(包含噪声点)
+    calculate_clustering_metrics(data, labels)
 
     # 可视化DBSCAN聚类结果
     visualize_dbscan_clustering_results(data, labels)
-
-    # 计算DBSCAN聚类指标
-    calculate_clustering_metrics(data, labels)
-
-    # 计算每一个簇包含的样本点个数
-    calculate_sample_numbers_of_each_cluster(labels)
 
 
 if __name__ == '__main__':
